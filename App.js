77994a8e7c3a1d956cf84c0aff49eaaf2648f45d
Tsanta Fitiavana <tsanta.ratolojanahary@versusmind.eu>
2023-12-08 21:31:25 +0300

    feat: 🚀 Add routing logic

--- App.js
+++ App.js
@@ -2,16 +2,25 @@ import { StatusBar } from "expo-status-bar"
 import { StyleSheet, Text, View } from "react-native"
 import { Login } from "./pages/login/Login"
 import { QueryClientProvider, QueryClient } from "react-query"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { Home } from "./pages/home/Home"
 
 export default function App() {
-  const queryClient = new QueryClient()
+    const queryClient = new QueryClient()
+    const Stack = createNativeStackNavigator();
 
     return (
         <QueryClientProvider client={queryClient}>
-            <View style={styles.container}>
-                <StatusBar style="auto" />
-                <Login />
-            </View>
+            <NavigationContainer>
+                <View style={styles.container}>
+                    <StatusBar style="auto" />
+                    <Stack.Navigator initialRouteName="Login">
+                        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+                        <Stack.Screen name="Home" component={Home} />
+                    </Stack.Navigator>
+                </View>
+            </NavigationContainer>
         </QueryClientProvider>
     )
 }
