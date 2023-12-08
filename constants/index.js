c88237d0b5689079ad59046cc4110c9d29ddb181
Tsanta Fitiavana <tsanta.ratolojanahary@versusmind.eu>
2023-12-08 22:38:51 +0300

    fix: Interchange color value

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,6 @@ export const BASE_URL = "http://ac.woogz.com/api/"
 export const AUTH_KEY = "4HOMJ300vn"
 export const COLOR = {
     error: "rgba(115, 29, 23, 0.5)",
-    neutral: "rgba(23, 115, 46, 0.5)",
-    success: "rgba(0, 0, 0, 0.5)",
+    neutral: "rgba(0, 0, 0, 0.5)",
+    success: "rgba(23, 115, 46, 0.5)",
 }
